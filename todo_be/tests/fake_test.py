d9ca3b0f8eae64665531caa981895b43dbb666ae
Lorenzo <lorenzo.dagostino@digitalsoft.com>
2024-08-28 17:35:58 +0200

    test: added unit test fake

---
+++ todo_be/tests/fake_test.py
@@ -0,0 +1,14 @@
+# Fake Test using pytest
+
+import pytest
+
+class TestExampleClass:
+    def test_example_method(self):
+        # Arrange
+        input_value = "test_input"
+
+        # Act
+        result = "test_input"
+
+        # Assert
+        assert result == input_value
