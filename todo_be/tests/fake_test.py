99cd033320a1605a1758517cd11e83863a552829
Lorenzo <lorenzo.dagostino@digitalsoft.com>
2024-08-28 17:37:44 +0200

    style: run formatter and linter

--- todo_be/tests/fake_test.py
+++ todo_be/tests/fake_test.py
@@ -1,14 +1,10 @@
-# Fake Test using pytest
-
-import pytest
-
 class TestExampleClass:
     def test_example_method(self):
         # Arrange
-        input_value = "test_input"
+        input_value = 'test_input'
 
         # Act
-        result = "test_input"
+        result = 'test_input'
 
         # Assert
         assert result == input_value
